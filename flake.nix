fa0bb52dc742244c952ba08fe29efe10b2a3e4a1
Vakiyama <vakiyama@outlook.com>
2024-10-06 00:29:50 -0700

    feat: basic interactions

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Next.js Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            bun
+            # nodejs_20
+          ];
+        };
+      }
+    );
+}
+
+
